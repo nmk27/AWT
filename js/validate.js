2e6b688af42686b0d39ac7978286618fa1533a5e
Niran Pradhan <namikaze.niran@gmail.com>
2021-12-05 18:20:51 +0530

    second commit

--- js/validate.js
+++ js/validate.js
@@ -1,2 +1,13 @@
-let username = document.getElementById("regno").value;
-console.log(username);
+function validate() {
+    const username = document.getElementById('regno').value;
+    const pass = document.getElementById('password').value;
+    if (username === "201900128") {
+        if (pass === "ggwp") {
+            document.querySelector('form').setAttribute("action", "home.html");
+        }
+        else
+            alert("Wrong Details");
+    }
+    else
+        alert("Wrong Details");
+}
